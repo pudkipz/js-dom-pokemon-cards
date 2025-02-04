b6ecc1c110051b75cc59ccfdaa300206cbf31ea7
Matilda Sönnergaard <matilda.sonnergaard@se.experis.com>
2025-02-04 13:18:40 +0100

    Break out code into functions

--- src/index.js
+++ src/index.js
@@ -1,25 +1,21 @@
-const cardsListUL = document.querySelector('.cards')
-cardsListUL.innerHTML = ''
-
-// const card = data[0]
-for (const card of data) {
-    const cardLi = document.createElement('li')
-    cardLi.setAttribute('id', card.id)
-    cardLi.setAttribute('class', 'card')
-
-    // header
+function getCardHeader(card) {
     const cardHeader = document.createElement('h2')
     cardHeader.setAttribute('class', 'card--title')
     // cardHeader.append(card.name)
     cardHeader.innerText = card.name
+    return cardHeader
+}
 
-    // image
+function getCardImage(card) {
     const cardImage = document.createElement('img')
     cardImage.setAttribute('width', '256')
     cardImage.setAttribute('class', 'card--img')
     cardImage.setAttribute('src', card.sprites.other['official-artwork'].front_default)
+    // cardImage.setAttribute('src', card.sprites.other.dream_world.front_default)
+    return cardImage
+}
 
-    // stats
+function getCardStats(card) {
     const statsListUL = document.createElement('ul')
     statsListUL.setAttribute('class', 'card--text')
 
@@ -28,24 +24,50 @@ for (const card of data) {
         statLi.innerText = stat.stat.name + ': ' + stat.base_stat
         statsListUL.appendChild(statLi)
     }
+    return statsListUL
+}
 
-    // game versions
-    const gamesListHeader = document.createElement('h3')
-    gamesListHeader.innerText = 'Has appeared in:'
+function getGameVersionList(card) {
     const gamesListUL = document.createElement('ul')
     gamesListUL.setAttribute('class', 'card--games')
+    
     for (const game of card.game_indices) {
         const gameLi = document.createElement('li')
         gameLi.innerText = game.version.name
         gamesListUL.appendChild(gameLi)
     }
-
-
-    cardLi.appendChild(cardHeader)
-    cardLi.appendChild(cardImage)
-    cardLi.appendChild(statsListUL)
-    cardLi.appendChild(gamesListHeader)
-    cardLi.appendChild(gamesListUL)
-
-    cardsListUL.appendChild(cardLi)
+    return gamesListUL
 }
+
+function renderCards(cardsListUL) {
+    cardsListUL.innerHTML = ''
+    // const card = data[0]
+    for (const card of data) {
+        const cardLi = document.createElement('li')
+        cardLi.setAttribute('id', card.id)
+        cardLi.setAttribute('class', 'card')
+
+        cardHeader = getCardHeader(card)
+        cardImage = getCardImage(card)
+        statsListUL = getCardStats(card)
+        gamesListUL = getGameVersionList(card)
+
+        const gamesListHeader = document.createElement('h3')
+        gamesListHeader.innerText = 'Has appeared in:'
+
+        cardLi.appendChild(cardHeader)
+        cardLi.appendChild(cardImage)
+        cardLi.appendChild(statsListUL)
+        cardLi.appendChild(gamesListHeader)
+        cardLi.appendChild(gamesListUL)
+
+        cardsListUL.appendChild(cardLi)
+    }
+}
+
+function main() {
+    const cardsListUL = document.querySelector('.cards')
+    renderCards(cardsListUL)
+}
+
+main()
